23551623f9c1fff2e310b9a010a6181efbc336f0
bkzhn <bkzhns@gmail.com>
2024-04-25 17:37:46 -0700

    Remove linux specific package manager update code

--- uuaa.sh
+++
@@ -1,4 +0,0 @@
-sudo nala update
-sudo nala upgrade -y
-sudo nala autoremove
-sudo apt autoclean
--- uuac.sh
+++
@@ -1,4 +0,0 @@
-sudo dnf update --refresh
-sudo dnf upgrade --refresh
-sudo dnf autoremove 
-sudo dnf clean all
