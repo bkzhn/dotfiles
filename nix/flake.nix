1cad1c89f9d1113c2cfd953f491c2f422a9a85fa
bkzhn <bkzhns@gmail.com>
2026-03-25 05:51:33 -0700

    Add flake for nix package manager

---
+++ nix/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "my-tools";
+        paths = [
+          pkgs.fastfetch
+        ];
+      };
+    };
+}
